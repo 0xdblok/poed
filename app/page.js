e27b806428179fd89bb645e00ad62af59aa91336
Natha <nathalenaat@Mac-mini-de-Natha.local>
2025-01-16 13:29:19 +0100

    ee

--- app/page.js
+++ app/page.js
@@ -66,7 +66,7 @@ export default function Home() {
           </a>
         </div>
         <div class="flex flex-col flex-1 min-h-[24px]">
-          <a href="">
+          <a href="https://t.me/+3ljqLkYb565mMmNl">
             {" "}
             <img
               loading="lazy"
